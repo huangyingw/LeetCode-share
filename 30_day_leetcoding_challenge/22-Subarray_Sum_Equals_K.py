dbe01189e1ab66f25a080e713f938c8a5457bfb1
Ying Huang <huangyingw@gmail.com>
2020-04-25 21:46:14 -0700

    n

--- 30_day_leetcoding_challenge/22-Subarray_Sum_Equals_K.py
+++ 30_day_leetcoding_challenge/22-Subarray_Sum_Equals_K.py
@@ -1,7 +1,6 @@
-### Using hashmap ###
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
+
         sum_from_head = {}
         Sum = 0
         total = 0
@@ -9,22 +8,20 @@ class Solution:
             Sum += nums[i]
             if Sum == k:
                 total += 1
-            total += sum_from_head.get( Sum-k, 0 )
-            sum_from_head[Sum] = sum_from_head.get( Sum, 0 ) + 1
-
+            total += sum_from_head.get(Sum - k, 0)
+            sum_from_head[Sum] = sum_from_head.get(Sum, 0) + 1
         return total
-        
 
-### Without space [Time Limit Exceeded] ###
+
 class Solution2:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
+
         total = 0
-        for start in range( len(nums) ):
+        for start in range(len(nums)):
             SUM = 0
-            for end in range( start, len(nums) ):
+            for end in range(start, len(nums)):
                 SUM += nums[end]
                 if SUM == k:
                     total += 1
-            
+
         return total
